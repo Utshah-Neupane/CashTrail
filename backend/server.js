826290d11ce2a5cbc31306a8614167a2ef408adb
Utshah Neupane <utshah777@gmail.com>
2025-08-25 13:46:45 -0500

    Routes added

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,7 @@ app.get("/", (req,res) => {
 });
 
 
+
 app.get("/api/transactions/:userId", async(req,res) => {
     try{
         const {userId} = req.params;
@@ -58,6 +59,7 @@ app.get("/api/transactions/:userId", async(req,res) => {
 
     }
 });
+
 
 
 app.post("/api/transactions", async (req, res) => {
@@ -107,6 +109,37 @@ app.delete("/api/transactions/:id", async (req,res) => {
         res.status(500).json({message:"Internal Server Error!"});
     }
 });
+
+
+
+app.get("/api/transactions/summary/:userId", async(req,res) =>{
+    try{
+        const {userId} = req.params;
+
+        const balanceResult = await sql`
+        SELECT COALESCE(SUM(amount),0) as balance FROM transactions WHERE user_id = ${userId}`;
+
+        const incomeResult = await sql`
+        SELECT COALESCE(SUM(amount),0) as income FROM transactions 
+        WHERE user_id = ${userId} AND amount > 0`;
+
+        const expensesResult = await sql`
+        SELECT COALESCE(SUM(amount),0) as expenses FROM transactions 
+        WHERE user_id = ${userId} AND amount < 0`;
+
+        res.status(200).json({
+            balance: balanceResult[0].balance,
+            income: incomeResult[0].income,
+            expenses: expensesResult[0].expenses
+        });
+
+    }
+    catch(error){
+        console.log("Error getting the summary", error);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+});
+
 
 
 
